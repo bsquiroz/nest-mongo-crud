500488ea7cd692189079e9a944cc2bcdb1068040
bsquiroz <bemq1605@gmail.com>
2024-09-03 15:24:19 -0500

    feature/add implement mongo

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PokemonModule } from './modules/pokemon/pokemon.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [PokemonModule],
+  imports: [
+    PokemonModule,
+    MongooseModule.forRoot('mongodb://localhost:27017/pokedex-bk'),
+  ],
 })
 export class AppModule {}
